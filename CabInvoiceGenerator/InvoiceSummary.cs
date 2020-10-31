8d031f94add98bbbfd2af2607816e52390971751
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-31 20:17:18 +0530

    [pranav] Add. Calculate the multiple fares of the ride

--- CabInvoiceGenerator/InvoiceSummary.cs
+++ CabInvoiceGenerator/InvoiceSummary.cs
@@ -23,30 +23,5 @@ namespace CabInvoiceGenerator
             this.totalFare = totalFare;
             this.averageFare = this.totalFare / this.numberOfRides;
         }
-
-        /// <summary>
-        /// Overriding Equals Method.
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public override bool Equals(object obj)
-        {
-            if (obj == null) return false;
-            if (!(obj is InvoiceSummary)) return false;
-
-            InvoiceSummary inputedObject = (InvoiceSummary)obj;
-            return this.numberOfRides == inputedObject.numberOfRides && this.totalFare == inputedObject.totalFare && this.averageFare == inputedObject.averageFare;
-        }
-
-        /// <summary>
-        /// Overriding GetHashCode Method.
-        /// </summary>
-        /// <returns></returns>
-        public override int GetHashCode()
-        {
-            return this.numberOfRides.GetHashCode() ^ this.totalFare.GetHashCode() ^ this.averageFare.GetHashCode();
-        }
     }
-
-
 }
