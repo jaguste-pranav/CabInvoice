8067dc41d305f540029dc929182950d6ef4d42bb
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-31 19:22:37 +0530

    [pranav] Add. CAlculate the fare of the ride

---
+++ canInvoiceGenTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using CabInvoiceGenerator;
+
+namespace canInvoiceGenTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void GivenRide_ShouldReturn()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+    }
+}
